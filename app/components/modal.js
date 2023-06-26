d34ecd53072fa0cc357d6b53ffdf9089a36fdc94
Priyam Shrivastava <priyam2007sp@gmail.com>
2023-06-26 21:55:28 +0530

    styled the modal

--- app/components/modal.js
+++ app/components/modal.js
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Modal(props) {
   const {show, onClose, product} = props
   if(show){
     return (
-      <div className=' fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
-        <div className=' bg-white p-2 rounded text-black'>
-          <h1>{product.title}</h1>
-          <h3>{product.price}</h3>
-          <button onClick={onClose}>X</button>
+      <div onClick={onClose} className=' fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+        <div className=' m-10 max-h-[500px] max-w-[600px] overflow-y-auto bg-zinc-800 p-9 rounded text-white grid grid-cols-1 items-center gap-5'>
+          <h1 className=' font-bold text-center text-xl'>Product Details:</h1>
+          <div className=' flex justify-center items-center'>
+            <Image className='w-34 h-52 rounded' src={product.image} alt={product.title} width={200} height={300}/>
+          </div>
+          <h1 className=' text-center font-semibold'>{product.title}</h1>
+          <h2 className=' text-center'><span className=' font-semibold'>Category:</span> {product.category}</h2>
+          <h3 className=' text-center'><span className=' font-semibold'>Price: </span>${product.price}</h3>
+          <h2 className=' text-center'><span className=' font-semibold'>Rating: </span><span className=' text-green-400'>{product.rating.rate}</span></h2>
+          <h2 className=' text-center'><span className=' font-semibold'>Description:</span> {product.description}</h2>
+          <button className='bg-white text-black p-2 rounded font-semibold hover:bg-slate-100 transition ease-linear duration-200' onClick={onClose}>Close X</button>
         </div>
       </div>
     )
