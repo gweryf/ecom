ccfdd6b42fdfd3abec141776cb31a35d3c0d5747
Priyam Shrivastava <priyam2007sp@gmail.com>
2023-06-26 20:56:57 +0530

    styled the front page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import Navbar from './components/navbar'
+import Link from 'next/link'
 export default function Home() {
   return (
-    <div>
+    <div className=' h-screen flex flex-col'>
       <Navbar/>
-      lmao
+      <section className=' h-fit w-full bg-gradient-to-r from-zinc-800 to-black flex-1 gap-14 flex flex-col justify-center items-center'>
+        <h1 className=' font-extrabold text-5xl'>E-Commerce</h1>
+        <Link href='/products'><button className=' px-3 py-1 text-xl font-semibold hover:bg-white hover:text-black'>VISIT THE SHOP</button></Link>
+      </section>
     </div>
   )
 }
