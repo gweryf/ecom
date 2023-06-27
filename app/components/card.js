9d5822913e86693594275af4d2bb3040fd4ef613
Priyam Shrivastava <priyam2007sp@gmail.com>
2023-06-27 14:19:21 +0530

    styled page and changed code to get api response

--- app/components/card.js
+++ app/components/card.js
@@ -16,7 +16,7 @@ export default function Card(props) {
   return (
     <div className=' flex flex-col justify-center items-center gap-1'>
       <div className=' flex flex-col justify-center items-center gap-5' onClick={onModal}>
-        <Image className=' rounded  w-64 h-80' src={product.image} alt={product.title} width={200} height={300}/>
+        <Image className=' rounded  w-64 h-80' src={product.images[0]} alt={product.title} width={200} height={300}/>
         <h2 className=' text-center font-bold text-lg'>{product.title}</h2>
         <h3 className=' text-center font-semibold text-lg'>Price: ${product.price}</h3>
       </div>
--- app/components/modal.js
+++ app/components/modal.js
@@ -5,15 +5,15 @@ export default function Modal(props) {
   if(show){
     return (
       <div onClick={onClose} className=' fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
-        <div className=' m-10 max-h-[500px] max-w-[600px] overflow-y-auto bg-zinc-800 p-9 rounded text-white grid grid-cols-1 items-center gap-5'>
+        <div className=' m-10 h-[500px] w-[600px] overflow-y-auto bg-zinc-800 p-9 rounded text-white grid grid-cols-1 items-center gap-5'>
           <h1 className=' font-bold text-center text-xl'>Product Details:</h1>
           <div className=' flex justify-center items-center'>
-            <Image className='w-34 h-52 rounded' src={product.image} alt={product.title} width={200} height={300}/>
+            <Image className='w-34 h-52 rounded' src={product.images[0]} alt={product.title} width={200} height={300}/>
           </div>
           <h1 className=' text-center font-semibold'>{product.title}</h1>
-          <h2 className=' text-center'><span className=' font-semibold'>Category:</span> {product.category}</h2>
+          <h2 className=' text-center'><span className=' font-semibold'>Category:</span> {product.category.name}</h2>
           <h3 className=' text-center'><span className=' font-semibold'>Price: </span>${product.price}</h3>
-          <h2 className=' text-center'><span className=' font-semibold'>Rating: </span><span className=' text-green-400'>{product.rating.rate}</span></h2>
+          {/* <h2 className=' text-center'><span className=' font-semibold'>Rating: </span><span className=' text-green-400'>{product.rating.rate}</span></h2> */}
           <h2 className=' text-center'><span className=' font-semibold'>Description:</span> {product.description}</h2>
           <button className='bg-white text-black p-2 rounded font-semibold hover:bg-slate-100 transition ease-linear duration-200' onClick={onClose}>Close X</button>
         </div>
